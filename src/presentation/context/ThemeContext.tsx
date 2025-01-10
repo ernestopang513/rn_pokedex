82d2aa5276bc9d34e08a5c2e4d664b869f03baf3
ErnestoLap <ernestopang513@hotmail.com>
2025-01-09 21:30:53 -0600

    Temas combinados

--- src/presentation/context/ThemeContext.tsx
+++ src/presentation/context/ThemeContext.tsx
@@ -1,13 +1,18 @@
 import { createContext, PropsWithChildren } from "react";
 
 import {
-  DefaultTheme,
     NavigationContainer,
     DarkTheme as NavigationDarkTheme,
     DefaultTheme as NavigationDefaultTheme,
-    Theme,
   } from '@react-navigation/native';
-  import { adaptNavigationTheme, PaperProvider } from 'react-native-paper';
+
+
+import { adaptNavigationTheme,
+    MD3DarkTheme,
+    MD3LightTheme,
+    PaperProvider 
+} from 'react-native-paper';
+
 import { useColorScheme } from "react-native";
   
   const { LightTheme, DarkTheme } = adaptNavigationTheme({
@@ -17,22 +22,52 @@ import { useColorScheme } from "react-native";
 
 
 export const ThemeContext = createContext({
-    isDark: true,
-    theme: LightTheme
+    isDark: false,
 });
+
+
 
 
 export const ThemeContextProvider = ({ children }: PropsWithChildren) => {
 
     const colorScheme = useColorScheme();
 
-    const isDarkTheme = colorScheme === 'dark';
-    const theme = isDarkTheme ? DarkTheme : LightTheme;
+    const isDark = colorScheme === 'dark';
 
+    const CombinedTheme = {
+      ...MD3LightTheme,
+      colors: {
+        ...(isDark ? DarkTheme.colors : LightTheme.colors),
+        ...(isDark ? MD3DarkTheme.colors : MD3LightTheme.colors ),
+      },
+      fonts: {
+        ...MD3DarkTheme.fonts,
+        ...NavigationDefaultTheme.fonts
+      },
+      dark: isDark,
+    }
+    
+
+    // console.log(JSON.stringify(LightTheme.colors.primary, null, 4))
+    // console.log('------------------------------------------------------')
+    // console.log(JSON.stringify(CombinedTheme.colors.primary, null, 3))
+    // console.log('------------------------------------------------------')
+    // console.log(JSON.stringify(MD3DarkTheme.colors.primary, null, 3))
+    // console.log('------------------------------------------------------')
+    // console.log(JSON.stringify(CombinedTheme.colors, null, 3))
+    // console.log({isDark})
+
+
+    console.log(JSON.stringify(NavigationDarkTheme, null, 3))
+    
     return (
-        <PaperProvider theme={theme} >
-            <NavigationContainer theme={{...DefaultTheme}}>
+        <PaperProvider theme={CombinedTheme}  >
+            <NavigationContainer theme={CombinedTheme}>
+            <ThemeContext.Provider value={{
+              isDark
+            }}>
                 {children}
+            </ThemeContext.Provider>
             </NavigationContainer>
         </PaperProvider>
     )
