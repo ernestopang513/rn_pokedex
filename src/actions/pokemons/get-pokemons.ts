a2101c73d5b39d5dbfae82128e30d0530932119e
ErnestoLap <ernestopang513@hotmail.com>
2025-01-13 14:27:01 -0600

    Interfaces de pokeApi

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,16 +1,28 @@
 import { pokeApi } from "../../config/api/pokeApi";
-import { Pokemon } from "../../domain/entities/pokemon";
+import type { Pokemon } from "../../domain/entities/pokemon";
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from "../../infrastructure/interfaces/pokeapi.interfaces";
+
+export const sleep = async() => {
+    return new Promise(resolve => setTimeout(resolve, 2000));
+}
 
 
-
-
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (page: number, limit : number = 20): Promise<Pokemon[]> => {
 
     try {
-        const url = '/pokemon';
+        const url = `/pokemon?offset=${page * 10 }&limit=${limit}`;
+        
 
-        const {data} = await pokeApi.get(url);
+        const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
+        const pokemonPromises = data.results.map( (info) => {
+            return pokeApi.get<PokeAPIPokemon>(info.url)
+        })
+
+        const pokeApiPokemon = await Promise.all(pokemonPromises)
+
+        console.log(JSON.stringify(pokeApiPokemon,null, 4))
+        
         console.log(JSON.stringify(data, null, 4));
         console.log(typeof(data));
 
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,18 +1,34 @@
-import { View } from "react-native"
+import { ActivityIndicator, View } from "react-native"
 import { Button, Text } from "react-native-paper"
 import { getPokemons } from "../../../actions/pokemons"
+import { useQuery } from "@tanstack/react-query"
 
 export const HomeScreen = () => {
 
-  getPokemons();
+  const { isLoading, data}  = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60 * 60
+  });
+
+  // getPokemons();
 
   return (
     <View>
         <Text>HomeScreen</Text>
 
+        { isLoading ?(
+        
+        <ActivityIndicator/>
+        
+        ) : (
+        
         <Button mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
+            Press me
+          </Button>
+
+        )}
+
     </View>
   )
 }
